3990961e2e618e191f88b781f0d30eea615e1788
kyokenshi <caloccon113@gmail.com>
2025-04-04 22:30:02 +0700

    feat: fix cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,16 +5,21 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import StyledJsxRegistry from '@/lib/registry';
 import { GlobalStyles } from './GlobalStyles';
-import { Metadata } from 'next';
-
+import './ckeditor.css';
 
 type Props = {
   children: ReactNode;
 };
 
-
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({ children }: Props) {
-  return <StyledJsxRegistry><AntdRegistry><GlobalStyles />{children}</AntdRegistry></StyledJsxRegistry>;
+  return (
+    <StyledJsxRegistry>
+      <AntdRegistry>
+        <GlobalStyles />
+        {children}
+      </AntdRegistry>
+    </StyledJsxRegistry>
+  );
 }
