ae81f51543ac94dc2061ebe1b0bf08c4659f6e71
kyokenshi <caloccon113@gmail.com>
2025-05-07 14:32:40 +0700

    feat: add social

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+
+'use client'
 import React from 'react';
 import { StyledFooter } from './styles';
 import Image from 'next/image';
@@ -55,32 +57,51 @@ const Footer = (props: Props) => {
                                 </div>
                             </div>
                         </div>
-                        {/* <Space
-                            size={16}
-                            direction="horizontal"
-                            style={{ display: 'flex', justifyContent: 'center', marginTop: 16 }}
-                        >
-                            <div className="flex gap-1 align-middle">
+                        <div className='flex gap-2 flex-col '>
+                            <Space
+                                size={16}
+                                direction="horizontal"
+                                style={{ display: 'flex', justifyContent: 'center', marginTop: 16 }}
+                            >
+                                <div className="flex gap-1 align-middle">
+                                    {dataSocial?.map((sc: any) => {
+                                        if (sc?.type === 'footer') {
+                                            return (
+                                                <Link
+                                                    key={sc.id}
+                                                    href={sc?.url}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                >
+                                                    <Image
+                                                        src={getImageUrl(sc?.image?.formats?.thumbnail?.url)}
+                                                        alt={sc?.name}
+                                                        width={24}
+                                                        height={24}
+                                                    />
+                                                </Link>
+                                            );
+                                        }
+                                    })}
+                                </div>
+                            </Space>
+                            <div className='flex justify-center '>
                                 {dataSocial?.map((sc: any) => {
-                                    if (sc?.type === 'footer') {
-                                        return (
-                                            <Link
-                                                href={sc?.url}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                            >
-                                                <Image
-                                                    src={getImageUrl(sc?.image?.formats?.thumbnail?.url)}
-                                                    alt={sc?.name}
-                                                    width={24}
-                                                    height={24}
-                                                />
-                                            </Link>
-                                        );
+                                    if (sc?.name === "wechat" && sc?.image?.formats?.thumbnail?.url) {
+                                        return <Image
+                                            key={sc.id}
+                                            src={getImageUrl(sc?.image?.formats?.thumbnail?.url)}
+                                            alt={sc?.name}
+                                            className='mt-2'
+                                            width={200}
+                                            height={200}
+                                        />
                                     }
                                 })}
                             </div>
-                        </Space> */}
+
+                        </div>
+
                     </div>
                     <div className='col-span-1 md:col-span-2'>
                         <iframe
